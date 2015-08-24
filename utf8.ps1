f9c073bee681acad182f313d1af077ed6b625994
Omar AL Zabir <omaralzabir@gmail.com>
2015-08-24 23:57:26 +0100

    Add translation popup and bangla translation

--- utf8.ps1
+++ utf8.ps1
@@ -1,3 +1,3 @@
-﻿gci page/page*.html | % { [string]$text = (gc $_) -join "`n"; sc -Encoding utf8 $_ $text }
+﻿gci page/*.html | % { [string]$text = (gc $_) -join "`n"; sc -Encoding utf8 $_ $text }
 gci page/*.js | % { [string]$text = (gc $_) -join "`n"; sc -Encoding utf8 $_ $text }
 
