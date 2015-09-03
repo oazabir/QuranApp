920db13b40867a342b4fcc15780914cb80bb4d20
Omar AL Zabir <omaralzabir@gmail.com>
2015-09-03 10:59:33 +0100

    Page content files utf8 converted for IE browsers

--- utf8.ps1
+++ utf8.ps1
@@ -1,3 +1,5 @@
 ﻿gci page/*.html | % { [string]$text = (gc $_) -join "`n"; sc -Encoding utf8 $_ $text }
 gci page/*.js | % { [string]$text = (gc $_) -join "`n"; sc -Encoding utf8 $_ $text }
 
+gci translations/bangla/*.html | % { [string]$text = (gc $_) -join "`n"; sc -Encoding utf8 $_ $text }
+gci translations/english/*.html | % { [string]$text = (gc $_) -join "`n"; sc -Encoding utf8 $_ $text }
