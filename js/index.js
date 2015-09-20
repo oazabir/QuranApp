658a383082f0e65bfcdc7aa28d6235191a8e45d7
Omar AL Zabir <omaralzabir@gmail.com>
2015-09-20 13:23:19 +0100

    Fix tooltip arrow css

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@
 
 QuranApp = (function() {
 	var $this = this;
-	var version = 1509201309;
+	var version = 1509201321;
 	var versionSuffix = "?v=" + version;
 		
 	/**************************************
