a4fab892d382a13780269968d4cec84d85104210
Omar AL Zabir <omaralzabir@gmail.com>
2015-10-10 16:05:14 +0100

    Update cache

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@
 
 QuranApp = (function() {
 	var $this = this;
-	var version = 1510041212;
+	var version = 1510101605;
 	var versionSuffix = "?v=" + version;
 		
 	/**************************************
