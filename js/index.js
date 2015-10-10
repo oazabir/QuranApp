73a678cd61154f07e8f90dc3f8d809cae65bf491
Omar AL Zabir <omaralzabir@gmail.com>
2015-10-10 18:02:12 +0100

    Cleaner UI

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@
 
 QuranApp = (function() {
 	var $this = this;
-	var version = 1510101800;
+	var version = 1510101802;
 	var versionSuffix = "?v=" + version;
 		
 	/**************************************
