992fd46bb4697e1638800086d1896212b9e158e5
Omar AL Zabir <omaralzabir@gmail.com>
2015-10-10 18:17:16 +0100

    Update screenshot

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@
 
 QuranApp = (function() {
 	var $this = this;
-	var version = 1510101806;
+	var version = 1510101817;
 	var versionSuffix = "?v=" + version;
 		
 	/**************************************
