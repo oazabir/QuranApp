86e9fc2826ebd4d5eaae6508fd4629b5dee9f472
Omar AL Zabir <omaralzabir@gmail.com>
2015-10-13 11:21:44 +0100

    Better font renderin for Chrome on Windows

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@
 
 QuranApp = (function($) {
 	var $this = this;
-	var version = 1510131119;
+	var version = 1510131121;
 	var versionSuffix = "?v=" + version;
 		
 	/**************************************
