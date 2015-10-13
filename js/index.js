ab50d20f5f2f95c3998f8dd434ad43c38a77c974
Omar AL Zabir <omaralzabir@gmail.com>
2015-10-13 11:28:37 +0100

    IE doing too much antiliasing

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@
 
 QuranApp = (function($) {
 	var $this = this;
-	var version = 1510131126;
+	var version = 1510131128;
 	var versionSuffix = "?v=" + version;
 		
 	/**************************************
