a321e10ab82918b26575c23fc761d7b27dd6fa99
Omar AL Zabir <omaralzabir@gmail.com>
2015-10-13 08:42:05 +0100

    Revert simple UI

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@
 
 QuranApp = (function() {
 	var $this = this;
-	var version = 1510101821;
+	var version = 1510130842;
 	var versionSuffix = "?v=" + version;
 		
 	/**************************************
